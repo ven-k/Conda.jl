a832bfdea71faa8d235a76882337882a43aa753d
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2022-02-22 09:42:42 +0530

    `condadir` and `condadeps` at init
    - This should help while "building" PyCall on a relocated sysimage

--- src/Conda.jl
+++ src/Conda.jl
@@ -455,4 +455,8 @@ function pip(cmd::AbstractString, pkgs::PkgOrPkgs, env::Environment=ROOTENV)
     nothing
 end
 
+function __init__()
+    global condadir = abspath(first(DEPOT_PATH), "conda")
+    global condadeps = joinpath(condadir, "deps.jl")
+end
 end
